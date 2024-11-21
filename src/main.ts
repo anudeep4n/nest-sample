10e945cf03c0ed5a94dfd6b3b6c5682da03ae54c
Lakanavarapu Naga Manikanta Anu deep <lakanavarapu.anudeep@lakanavarapu.anudeep>
2024-11-21 12:38:47 +0530

    swagger changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from 'swagger-host';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app, { endpoint: '/.well-known/swagger', file: 'swagger.yaml' });
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
